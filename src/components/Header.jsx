bf0559670f1fd6a534cf324d33097a7932dd4e52
Suzy-Lee <>
2024-06-05 19:26:42 +0900

    feat: add dark mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import MenuItem from './MenuItem';
 import { AiFillHome } from 'react-icons/ai';
 import { BsFillInfoCircleFill } from 'react-icons/bs';
+import DarkModeSwitch from './DarkModeSwitch';
 
 export default function Header() {
   return (
@@ -10,12 +11,15 @@ export default function Header() {
         <MenuItem title='home' address='/' Icon={AiFillHome} />
         <MenuItem title='about' address='/about' Icon={BsFillInfoCircleFill} />
       </div>
-      <Link href={'/'} className='flex items-center gap-1'>
-        <span className='px-2 py-1 text-2xl font-bold rounded-lg bg-amber-500'>
-          IMDb
-        </span>
-        <span className='hidden text-xl sm:inline'>Clone</span>
-      </Link>
+      <div className='flex items-center gap-4'>
+        <DarkModeSwitch />
+        <Link href={'/'} className='flex items-center gap-1'>
+          <span className='px-2 py-1 text-2xl font-bold rounded-lg bg-amber-500'>
+            IMDb
+          </span>
+          <span className='hidden text-xl sm:inline'>Clone</span>
+        </Link>
+      </div>
     </div>
   );
 }
