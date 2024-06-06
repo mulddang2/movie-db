14f233164404e065ca4952e2b85eae3d573ccd0d
Suzy-Lee <>
2024-06-06 19:09:52 +0900

    feat: create the navebar component

---
+++ src/components/NavbarItem.jsx
@@ -0,0 +1,26 @@
+'use client';
+
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
+
+export default function NavbarItem({ title, param }) {
+  const searchParams = useSearchParams();
+  const genre = searchParams.get('genre');
+
+  console.log('param ->>' + param);
+  console.log('genre ->>' + genre);
+  return (
+    <div>
+      <Link
+        href={`/?genre=${param}`}
+        className={`hover:text-amber-600 font-semibold ${
+          genre === param
+            ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg'
+            : ''
+        }`}
+      >
+        {title}
+      </Link>
+    </div>
+  );
+}
