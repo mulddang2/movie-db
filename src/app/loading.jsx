9fbd1d77a533c298a620e83eb2def8bde4d15e14
Suzy-Lee <>
2024-06-11 15:33:12 +0900

    feat: add loading effect using nextjs

---
+++ src/app/loading.jsx
@@ -0,0 +1,7 @@
+export default function loading() {
+  return (
+    <div className='flex justify-center mt-44'>
+      <img className='h-16' src='spinner.svg' alt='loading...' />
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,28 @@
-import Results from "@/components/Results";
+import Results from '@/components/Results';
 
 const API_KEY = process.env.API_KEY;
 
 export default async function Home({ searchParams }) {
   const genre = searchParams.genre || 'fetchTrending';
-  const res = await fetch(`https://api.themoviedb.org/3${
-    genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
-  }?api_key=${API_KEY}&language=ko-KR&page=1
-  }`);
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=ko-KR&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+
+  // NOTE: loading 컴포넌트 테스트
+  // const res = await new Promise((resolve) => {
+  //   setTimeout(async () => {
+  //     const response = await fetch(
+  //       `https://api.themoviedb.org/3${
+  //         genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
+  //       }?api_key=${API_KEY}&language=ko-KR&page=1`,
+  //       { next: { revalidate: 10 } }
+  //     );
+  //     resolve(response);
+  //   }, 2000);
+  // });
 
   const data = await res.json();
 
