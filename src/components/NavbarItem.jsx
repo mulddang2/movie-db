cc668241e5fc140ea88fa0861c17542266cf9665
Suzy-Lee <>
2024-06-15 00:36:17 +0900

    fix: wrap in a suspense boundary at Navbar page

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -2,26 +2,23 @@
 
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
-import { Suspense } from 'react';
 
 export default function NavbarItem({ title, param }) {
   const searchParams = useSearchParams();
   const genre = searchParams.get('genre');
 
   return (
-    <Suspense>
-      <div>
-        <Link
-          href={`/?genre=${param}`}
-          className={`hover:text-amber-600 font-semibold ${
-            genre === param
-              ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg'
-              : ''
-          }`}
-        >
-          {title}
-        </Link>
-      </div>
-    </Suspense>
+    <div>
+      <Link
+        href={`/?genre=${param}`}
+        className={`hover:text-amber-600 font-semibold ${
+          genre === param
+            ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg'
+            : ''
+        }`}
+      >
+        {title}
+      </Link>
+    </div>
   );
 }
