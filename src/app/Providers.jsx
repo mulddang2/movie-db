4e560515f7e15811b80324947cc979348b46f54a
Suzy-Lee <>
2024-06-19 11:59:06 +0900

    style: 기본 모드를 다크모드로 변경

--- src/app/Providers.jsx
+++ src/app/Providers.jsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from 'next-themes';
 
 export default function Providers({ children }) {
   return (
-    <ThemeProvider defaultTheme='system' attribute='class'>
+    <ThemeProvider defaultTheme='dark' attribute='class'>
       <div className='min-h-screen text-gray-700 transition-colors duration-300 select-none dark:text-gray-200 dark:bg-gray-700'>
         {children}
       </div>
