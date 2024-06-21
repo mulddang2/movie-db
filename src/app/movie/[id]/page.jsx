127c4c63e563cff4ea3a355ef593811e76b91a0b
Suzy-Lee <>
2024-06-21 17:21:26 +0900

    fix: api key 접근 오류로 변수 앞에 NEXT_PUBLIC 추가

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default async function MoviePage({ params }) {
   const movieId = params.id;
   const res = await fetch(
-    `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.API_KEY}&language=ko-KR`
+    `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=ko-KR`
   );
   const movie = await res.json();
   return (
